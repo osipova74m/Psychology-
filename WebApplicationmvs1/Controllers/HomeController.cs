2da570bb18a7c135a2c3c6e9b58349ff26608abe
Maria Osipova <osipova74m@gmail.com>
2021-07-04 22:45:46 +0300

    Added example view

--- WebApplicationmvs1/Controllers/HomeController.cs
+++ WebApplicationmvs1/Controllers/HomeController.cs
@@ -3,28 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplicationmvs1.Models;
 
 namespace WebApplicationmvs1.Controllers
 {
     public class HomeController : Controller
     {
+        //PostContext pc;  ошиб в назв
         public ActionResult Index()
         {
-            return View();
+            var model = new List<PostText>();
+            // example of view render a list
+            //todo: transform this into context call (get from database)
+            PostText pt = new PostText
+            {
+                Body = "fggfjjiutytygdfd",
+                Name = "Perseptoin",
+                Id = Guid.NewGuid()
+            };
+            
+            PostText pt2 = new PostText
+            {
+                Body = "test cont",
+                Name = "lsd",
+                Id = Guid.NewGuid()
+            };
+
+            model.Add(pt);
+            model.Add(pt2);
+            
+            
+            return View(model);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
